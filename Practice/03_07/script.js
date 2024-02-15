f4b494593072b0ceaeb67d60e66bfac345f55401
azharz <zohaib.azhar@netsoltech.com>
2024-02-15 13:00:14 +0500

    Nested objects created and inspected in browser console

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,12 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const bagObj = {
+  name: "myBag",
+  size: "smallSize",
+  comprtments: {
+    mainComponent: "forLaptop",
+    smallComponent: "forTab",
+  },
+};
